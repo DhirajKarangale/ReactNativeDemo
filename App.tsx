b42a746aee61739cd6710c40bfef518dd22f5b0d
dhirajkarangale <dakarangale02@gmail.com>
2025-01-14 17:51:38 +0530

    SSL Pinning

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import { Button, View } from 'react-native';
 import FooterNavBar from './Components/FooterNavBar';
 import TabNavigator from './Components/TabNavigator';
+import { fetch } from 'react-native-ssl-pinning';
 
 function App(): React.JSX.Element {
 
+  const base_url = "https://192.168.1.12:1337";
+
+  function Test() {
+    fetch(`${base_url}/test`
+      , {
+        method: 'GET',
+        sslPinning: { certs: ['server'], }
+      }
+    )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   return (
     <>
-      <TabNavigator />
+
+      <View style={{ marginTop: 100 }}>
+        <Button
+          onPress={Test}
+          title="Test API"
+          color="#841584"
+        />
+      </View>
+
+      {/* <TabNavigator /> */}
       {/* <FooterNavBar /> */}
     </>
   );
